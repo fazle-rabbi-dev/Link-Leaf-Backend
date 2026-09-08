10eb8d1a2651b68fa5a2e9109ddb1adcd1d28135
fazle-rabbi-dev <frabby983@gmail.com>
2026-09-08 11:26:54 +0600

    fix: set sameSite to none for sessionId cookie

--- src/controllers/v1/auth.controller.ts
+++ src/controllers/v1/auth.controller.ts
@@ -190,7 +190,7 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
 	res.cookie("sessionId", existingUser.auth.sessions[existingUser.auth.sessions.length - 1]?._id, {
 		httpOnly: true,
 		secure: true,
-		sameSite: "strict",
+		sameSite: "none",
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 	});
 
@@ -326,7 +326,7 @@ export const socialLogin = asyncHandler(async (req: Request, res: Response) => {
 	res.cookie("sessionId", user.auth.sessions[user.auth.sessions.length - 1]?._id, {
 		httpOnly: true,
 		secure: true,
-		sameSite: "strict",
+		sameSite: "none",
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 	});
 
